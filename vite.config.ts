e61cd4b29355d483e05fc7cd7de722dc4e221551
Tilandky <lueng1314@gmail.com>
2025-12-16 08:57:27 +0800

    Remove conflicting jsx-loc plugin and add vite-plugin-svgr

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 // vite.config.ts (最終修正：使用 ESM 語法，解決 Dynamic require 錯誤)
 
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from 'vite'
 import path from "path";
 import react from "@vitejs/plugin-react";
-import svgr from "vite-plugin-svgr";
+import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
